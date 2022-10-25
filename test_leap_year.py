9992c9c4b74bac1b2d4c2b5b069a117f61543610
radwanhmadi <radwan.hmadi@hiof.no>
2022-10-25 16:03:39 +0200

    test leap year

---
+++ test_leap_year.py
@@ -0,0 +1,25 @@
+from leap_year import isLeapYear
+
+
+def test_is_divisible_by_4():
+    assert isLeapYear(4) == True
+
+
+def test_is_not_divisible_by_100():
+    assert isLeapYear(100) == False
+
+
+def test_is_divisible_by_400():
+    assert isLeapYear(400) == True
+
+
+def test_is_not_divisible_by_4():
+    assert isLeapYear(4) == True
+
+
+def test_is_divisible_by_100():
+    assert isLeapYear(100) == False
+
+
+def test_is_not_divisible_by_400():
+    assert isLeapYear(400) == True
